4072c4f5553e8dad8533a88230eca6f097a7bd21
gwdoit <danielg.pleroma@gmail.com>
2017-04-25 21:47:42 -0700

    hello

---
+++ src/edu/oit/lesson1/Hello.java
@@ -0,0 +1,10 @@
+package edu.oit.lesson1;
+
+public class Hello {
+	public static void test(){
+		System.out.println("static test");
+	}
+	public void test1(){
+		System.out.println("nonstatic test1");
+	}
+}
---
+++ src/edu/oit/lesson1/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.oit.lesson1;
+
+public class HelloWorld {
+	public static void main(String[] args) { 
+		
+		Hello.test();
+		new Hello().test1();
+        System.out.println("Hello, World! ");
+   }
+	
+}
